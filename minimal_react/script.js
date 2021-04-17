078b78a30fd8c95cf1dec5a6bde4ddb1e016f227
iansedano <iansedano@gmail.com>
2021-04-17 09:49:47 +0200

    init

---
+++ minimal_react/script.js
@@ -0,0 +1,6 @@
+ReactDOM.render(
+    React.createElement("h1", {}, "Hello World!"),
+    document.getElementById('root')
+)
+
+// https://github.com/shinglyu/minimal-react/blob/master/index.html
---
+++ minimal_react_JSX/script.js
@@ -0,0 +1,10 @@
+function HelloWorld () {
+	return <h1>Hello World</h1>
+}
+
+
+const root = document.getElementById("root");
+
+ReactDOM.render(<HelloWorld/>, root)
+
+// https://github.com/shinglyu/minimal-react/blob/master/index.html
